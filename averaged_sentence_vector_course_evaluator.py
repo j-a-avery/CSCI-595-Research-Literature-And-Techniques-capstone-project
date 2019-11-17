Attempted bug fixes for AveragedSentenceVectorCourseEvaluator. Averaged sentence vectors either don't work or need a better model.

averaged_sentence_vector_course_evaluator.py
@@ -8,6 +8,9 @@ import logging
 
 
 class AveragedSentenceVectorCourseEvaluator(EmbeddingCourseEvaluator):
+    def __init__(self, vector_file):
+        super().__init__(vector_file)
+
     def _calculate_similarity(self, course1: Course, course2: Course):
         """
         Calculates similarity between two sentences by calculating cosine
@@ -22,12 +25,16 @@ class AveragedSentenceVectorCourseEvaluator(EmbeddingCourseEvaluator):
         tokens2 = self.preprocess(course2.description)
 
         vector1 = np.array([self._wv[token] for token in tokens1])
+        logging.debug(f"{course1.id} shape = {vector1.shape}")
         vector2 = np.array([self._wv[token] for token in tokens2])
+        logging.debug(f"{course2.id} shape = {vector2.shape}")
 
         course1_mean = vector1.mean(axis=0)
+        logging.debug(f"{course1.id} mean = {course1_mean}")
         course2_mean = vector2.mean(axis=0)
+        logging.debug(f"{course2.id} mean = {course2_mean}")
 
-        cossim = 1-cosine(course1_mean, course2_mean)
+        cossim = 1 - cosine(course1_mean, course2_mean)
 
         if vector2.size == 0:
             logging.warning(f"Course: {course2}")